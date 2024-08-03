cbdd10ed9200aa06bb6130dabdea3c52d03420e7
Lance Nelson <lancen04@gmail.com>
2024-08-02 22:03:46 -0400

    started to create times_applied method

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,7 @@
 class Coupon < ApplicationRecord
   has_many :invoices
   belongs_to :merchant
+  has_many :transactions, through: :invoices
 
   validates :name, presence: true
   validates :code, presence: true, uniqueness: { case_sensitive: false }
@@ -9,4 +10,10 @@ class Coupon < ApplicationRecord
 
   enum discount_type: { percentage: 0, dollar: 1 }
   enum status: { inactive: 0, active: 1 }
+
+  def times_applied
+    transactions
+      .where('result = ?', 1)
+      .count
+  end
 end
